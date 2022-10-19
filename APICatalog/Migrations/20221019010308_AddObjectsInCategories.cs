aeebe5611e18e94b2a320d96e5e8a0c3bd8f472e
Pedrolustosa <pedroeternalss@gmail.com>
2022-10-18 22:07:32 -0300

    feat: Populating the Categories table

---
+++ APICatalog/Migrations/20221019010308_AddObjectsInCategories.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalog.Migrations
+{
+    public partial class AddObjectsInCategories : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Categories(Name, ImageURL) VALUES('Bebidas','bebidas.jpg')");
+            mb.Sql("INSERT INTO Categories(Name, ImageURL) VALUES('Lances','lances.jpg')");
+            mb.Sql("INSERT INTO Categories(Name, ImageURL) VALUES('Sobremesas','sobremesas.jpg')");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categories");
+        }
+    }
+}
