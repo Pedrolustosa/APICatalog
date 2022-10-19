d1b062f6dc6c8462f7a20fac08ec1ec5ead396a5
Pedrolustosa <pedroeternalss@gmail.com>
2022-10-18 21:53:12 -0300

    feat: Introducing the Data Annotations feature          Applying Data Annotations to Entity Properties

--- APICatalog/Models/Category.cs
+++ APICatalog/Models/Category.cs
@@ -1,4 +1,6 @@
-﻿namespace APICatalog.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalog.Models
 {
     public class Category
     {
@@ -7,9 +9,19 @@
             Products = new List<Product>();
         }
 
+        [Key]
         public int CategoryId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Nome")]
         public string? Name { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Imagem")]
         public string? ImageURL { get; set; }
+
         public ICollection<Product>? Products { get; set; }
     }
 }
--- APICatalog/Models/Product.cs
+++ APICatalog/Models/Product.cs
@@ -1,15 +1,42 @@
-﻿namespace APICatalog.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalog.Models
 {
     public class Product
     {
+        [Key]
         public int ProductId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Imagem")]
         public string? Name { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Descrição")]
         public string? Description { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        [Display(Name = "Preço")]
         public decimal Price { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Imagem")]
         public string? ImageURL { get; set; }
+
+        [Display(Name = "Estoque")]
         public float Inventory { get; set; }
+
         public DateTime CreationDate { get; set; }
+
         public int CategoryId { get; set; }
+
+        [Required]
+        [Display(Name = "Categoria")]
         public Category? Category { get; set; }
     }
 }
